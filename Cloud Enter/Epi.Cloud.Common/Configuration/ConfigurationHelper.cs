444845c7d08f725a75575f2ddc9720e66032888c
Garry Lenz <garry_lenz@sra.com>
2016-06-10 15:47:12 -0400

    Another missing file

---
+++ Cloud Enter/Epi.Cloud.Common/Configuration/ConfigurationHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Epi.Cloud.Common.Configuration
+{
+    public static class ConfigurationHelper
+    {
+        public static string GetEnvironmentResourceKey(string resourceName)
+        {
+            var environmentKey = ConfigurationManager.AppSettings["Environment"];
+            if (resourceName != null)
+            {
+                environmentKey = ConfigurationManager.AppSettings["Environment/" + resourceName] ?? environmentKey;
+            }
+            return string.IsNullOrWhiteSpace(environmentKey)? resourceName : resourceName + "@" + environmentKey ;
+        }
+    }
+}
