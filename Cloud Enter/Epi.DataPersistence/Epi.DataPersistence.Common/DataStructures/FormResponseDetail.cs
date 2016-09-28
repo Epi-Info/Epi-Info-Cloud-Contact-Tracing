2d3a529c6c9b32ee2354b07287e0ea8249f3816b
Garry Lenz <garry_lenz@sra.com>
2016-09-28 14:19:41 -0400

    Refactor projects to provide reusable projects for FormMetadata and DataPersistence

---
+++ Cloud Enter/Epi.DataPersistence/Epi.DataPersistence.Common/DataStructures/FormResponseDetail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epi.DataPersistence.DataStructures
+{
+    public partial class FormResponseDetail
+    {
+        public string GlobalRecordID { get; set; }
+        public string FormId { get; set; }
+        public string FormName { get; set; }
+        public int RecStatus { get; set; }
+        public string ParentFormId { get; set; }
+        public string RelateParentResponseId { get; set; }
+        public string FirstSaveLogonName { get; set; }
+        public string LastSaveLogonName { get; set; }
+        public DateTime FirstSaveTime { get; set; }
+        public DateTime LastSaveTime { get; set; }
+        public int LastActiveUserId { get; set; }
+        public bool IsRelatedView { get; set; }
+        public bool IsDraftMode { get; set; }
+        public List<int> PageIds { get; set; }
+
+        public int LastPageVisited { get; set; }
+
+        public string RequiredFieldsList { get; set; }
+        public string HiddenFieldsList { get; set; }
+        public string HighlightedFieldsList { get; set; }
+        public string DisabledFieldsList { get; set; }
+        public List<PageResponseDetail> PageResponseDetailList{ get; private set; }
+
+        public List<FormResponseDetail> ChildFormResponseDetailList { get; private set; }
+    }
+
+}
