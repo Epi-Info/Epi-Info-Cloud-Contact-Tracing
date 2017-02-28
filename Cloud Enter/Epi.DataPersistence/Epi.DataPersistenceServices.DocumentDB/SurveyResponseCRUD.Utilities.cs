636baade3303ab00747b8476533ce93773663110
Garry Lenz <garry_lenz@sra.com>
2017-02-28 10:51:35 -0500

    Update SurveyResponseCRUD.Utilities to accept Azure Portal generated connection strings for DocumentDB.

--- Cloud Enter/Epi.DataPersistence/Epi.DataPersistenceServices.DocumentDB/SurveyResponseCRUD.Utilities.cs
+++ Cloud Enter/Epi.DataPersistence/Epi.DataPersistenceServices.DocumentDB/SurveyResponseCRUD.Utilities.cs
@@ -31,11 +31,21 @@ namespace Epi.DataPersistenceServices.DocumentDB
         private void ParseConnectionString()
         {
             var connectionStringName = ConfigurationHelper.GetEnvironmentResourceKey("CollectedDataConnectionString");
-            var connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+            var connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString.Trim();
 
-            var parts = connectionString.Split(',');
-            _serviceEndpoint = parts[0].Trim();
-            for (var i = 1; i < parts.Length; ++i)
+            string[] parts;
+
+            if (connectionString.StartsWith("AccountEndpoint="))
+            {
+                parts = connectionString.Split(';');
+            }
+            else
+            {
+                connectionString = string.Concat("AccountEndpoint=", connectionString);
+                parts = connectionString.Split(',');
+            }
+
+            for (var i = 0; i < parts.Length; ++i)
             {
                 var nvp = parts[i];
                 var eqSignIndex = nvp.IndexOf('=');
@@ -43,6 +53,12 @@ namespace Epi.DataPersistenceServices.DocumentDB
                 var value = nvp.Substring(eqSignIndex + 1).Trim();
                 switch (name)
                 {
+                    case "accountendpoint":
+                        _serviceEndpoint = value.Trim();
+                        break;
+                    case "accountkey":
+                        _authKey = value;
+                        break;
                     case "authkey":
                         _authKey = value;
                         break;
@@ -71,11 +87,11 @@ namespace Epi.DataPersistenceServices.DocumentDB
 #else
                     if (AppSettings.GetBoolValue(AppSettings.Key.IsLocalReleaseBuild))
                     {
-                        _client = new DocumentClient(new Uri(_serviceEndpoint), _authKey);
+                        _client = new DocumentClient(new Uri(_serviceEndpoint), _authkey);
                     }
                     else
                     {
-                        _client = new DocumentClient(new Uri(_serviceEndpoint), _authKey,
+                        _client = new DocumentClient(new Uri(_serviceEndpoint), _authkey,
                             new ConnectionPolicy
                             {
                                 ConnectionMode = ConnectionMode.Direct,
